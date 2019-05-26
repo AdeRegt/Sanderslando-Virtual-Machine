b69f122a77f4f13a6fddf40a82efd3564fc02dfe
AdeRegt <AdeRegt@github.com>
2019-05-26 18:51:36 +0200

    adding more features

--- svm.c
+++ svm.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #define MEMORYSIZE 0xFFFF
+#define STACKSIZE  0xFF
 #define RET_EXIT    0
 #define RET_NO_EXIT 1
 #define RET_NO_ARG  2
@@ -12,10 +13,28 @@ unsigned char register_A = 0;
 unsigned char register_CP = 0;
 unsigned char register_SP = 0;
 unsigned char cmpbyte = 0;
+unsigned short stack[STACKSIZE];
 
 void panic(const char* message){
 	printf("[PANIC] %s \n",message);
 	exit(RET_PANIC);
+}
+
+void pushStack(){
+	if(register_SP<STACKSIZE){
+		stack[register_SP++] = register_CP;
+	}else{
+		panic("Stack overflow");
+	}
+}
+
+void popStack(){
+	if(register_SP>0){
+		register_SP--;
+		register_CP = stack[register_SP--];
+	}else{
+		panic("Stack underflow");
+	}
 }
 
 unsigned char getMemory(unsigned short location){
@@ -55,6 +74,9 @@ void reset(){
 	memory = (unsigned char *)malloc(MEMORYSIZE);
 	for(int i = 0 ; i < MEMORYSIZE ; i++){
 		memory[i] = 0x00;
+	}
+	for(int i = 0 ; i < STACKSIZE ; i++){
+		stack[i] = 0;
 	}
 	register_A = 0;
 	register_CP = 0;
@@ -174,6 +196,143 @@ int main(int argc,char** argv){
 			}else{
 				cmpbyte = 0;
 			}
+		}else if(opcode==0x08){
+			//
+			// Jump if equal
+			// JEQ
+			// 0x08 LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+			if(cmpbyte==1){
+				register_CP = loc;
+			}
+		}else if(opcode==0x09){
+			//
+			// Jump if cmp is not equal
+			// QNE
+			// 0x09 LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+			if(cmpbyte!=1){
+				register_CP = loc;
+			}
+		}else if(opcode==0x0A){
+			//
+			// Jump if cmp is more
+			// JMO
+			// 0x0A LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+                        if(cmpbyte==2){
+                                register_CP = loc;
+                        }
+		}else if(opcode==0x0B){
+			//
+                        // Jump if cmp is not more
+                        // JNM
+                        // 0x0B LOCATION
+                        //
+
+                        unsigned short loc = getNextWord();
+                        if(cmpbyte!=2){
+                                register_CP = loc;
+                        }
+
+		}else if(opcode==0x0C){
+                        //
+                        // Jump if cmp is less
+                        // JLE
+                        // 0x0C LOCATION
+                        //
+
+                        unsigned short loc = getNextWord();
+                        if(cmpbyte==3){
+                                register_CP = loc;
+                        }
+                }else if(opcode==0x0D){
+                        //
+                        // Jump if cmp is not less
+                        // JNL
+                        // 0x0D LOCATION
+                        //
+
+                        unsigned short loc = getNextWord();
+                        if(cmpbyte!=3){
+                                register_CP = loc;
+                        }
+
+                }else if(opcode==0x0E){
+			//
+			// jump
+			// JMP
+			// 0x0E LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+			register_CP = loc;
+		}else if(opcode==0x0F){
+			//
+			// call
+			// CAL
+			// 0x0F LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+			pushStack();
+			register_CP = loc;
+		}else if(opcode==0x10){
+			//
+			// return
+			// RET
+			// 0x10 
+			//
+
+			popStack();
+		}else if(opcode==0x11){
+			//
+			// shift A to the left
+			// LEF
+			// 0x11
+			//
+
+			register_A = register_A << 1;
+		}else if(opcode==0x12){
+                        //
+                        // shift A to the right
+                        // RIG
+                        // 0x12
+                        //
+
+                        register_A = register_A >> 1;
+                }else if(opcode==0x13){
+			//
+			// AND
+			// AND
+			// 0x13 LOCATION
+			//
+
+			unsigned short loc = getNextWord();
+			register_A &= getMemory(loc);
+		}else if(opcode==0x14){
+                        //
+                        // OR
+                        // ORM
+                        // 0x13 LOCATION
+                        //
+
+                        unsigned short loc = getNextWord();
+                        register_A |= getMemory(loc);
+                }else if(opcode==0x15){
+			//
+			// SYSTEMCALL
+			// SYS
+			// 0x15 LOCATION
+			//
+			unsigned short loc = getNextWord();
+			panic("SYSTEMCALL");
 		}else{
 			printf("[PANIC] Unrecognised token %x \n",opcode);
 			panic("CPU detected unrecognised token");
